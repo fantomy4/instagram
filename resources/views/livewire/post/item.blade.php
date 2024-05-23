ccadbb64d0bfce2e4c1be701e862e7c36a72de16
Dmytro <ytfantom1@gmail.com>
2024-05-23 20:37:45 +0300

    add swiper

--- resources/views/livewire/post/item.blade.php
+++ resources/views/livewire/post/item.blade.php
@@ -22,7 +22,58 @@
     <!-- main -->
     <main>
         <div class="my-2">
-            <x-video></x-video>
+            <!-- Slider main container -->
+            <div x-init="
+
+            new Swiper($el,{
+
+                modules: [Navigation, Pagination],
+                loop:true,
+
+                pagination: {
+                    el: '.swiper-pagination',
+                },
+
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                },
+
+            });
+            
+            " class="swiper h-[500px] border bg-white">
+                <!-- Additional required wrapper -->
+                <div class="swiper-wrapper">
+                    <!-- Slides -->
+                    <div class="swiper-slide"><x-video></x-video></div>
+                    <div class="swiper-slide"><img src="https://cdn.pixabay.com/photo/2017/08/13/08/22/balloon-2636520_1280.jpg" alt="" class="h-[500px] w-full block object-scale-down"></div>
+                    <div class="swiper-slide"><img src="https://cdn.pixabay.com/photo/2021/01/06/12/14/church-5894267_1280.jpg" alt="" class="h-[500px] w-full block object-scale-down"></div>
+                    ...
+                </div>
+                <!-- If we need pagination -->
+                <div class="swiper-pagination"></div>
+
+                <!-- prev -->
+                <div class="swiper-button-prev absolute top-1/2 z-10 p-2">
+                    <div class="bg-white/95 border p-1 rounded-full text-gray-900">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.8" stroke="currentColor" class="w-4 h-4">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+                        </svg>
+                    </div>
+                </div>
+
+                <!-- next -->
+                <div class="swiper-button-next absolute right-0 top-1/2 z-10 p-2">
+                    <div class="bg-white/95 border p-1 rounded-full text-gray-900">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                        </svg>
+                    </div>
+                </div>
+
+                <!-- If we need scrollbar -->
+                <div class="swiper-scrollbar"></div>
+            </div>
         </div>
     </main>
 
